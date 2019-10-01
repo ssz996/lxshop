5890cf5cfb85af98c1f69f0b56d3b6b179ca871b
ssz996 <zss_super@163.com>
2019-10-02 00:07:35 +0800

    second commit

---
+++ src/main/java/com/lxshop/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.lxshop.dao;
+
+import com.lxshop.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
